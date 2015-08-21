7f3f72ac905ed6cc448aa0b1e7e3c6b0864f6fe0
Pallav Aggarwal <pallav.aggarwal@gmail.com>
2015-08-21 18:35:02 +0530

    UART0 RXTX
    New project to test UART0 send and receive data

---
+++ UART0 RXTX/RTE/RTE_Components.h
@@ -0,0 +1,14 @@
+
+/*
+ * Auto generated Run-Time-Environment Component Configuration File
+ *      *** Do not modify ! ***
+ *
+ * Project: 'UART0 RXTX' 
+ * Target:  'UART0 RXTX' 
+ */
+
+#ifndef RTE_COMPONENTS_H
+#define RTE_COMPONENTS_H
+
+
+#endif /* RTE_COMPONENTS_H */
